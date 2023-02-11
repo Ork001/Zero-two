c1e7c1fc83640c3ad73c21053686b9f908da6f25
Y.I.N <109145190+Ork001@users.noreply.github.com>
2023-02-11 22:40:35 +0530

    Update support.js

--- Commands/Core/support.js
+++ Commands/Core/support.js
@@ -3,12 +3,12 @@ module.exports = {
     alias: ["supportgc"],
     desc: "Sends support group link.",
     cool:3,
-    react: "🍁",
+    react: "📡",
     category: "Core",
     start: async(Miku, m,{pushName}) => {
         m.reply(`Check your DM *${pushName}* Senpai !\n\nI have sent you support group link personally.`)
         let botpic = botImage1
-        let txt = `      🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Atlas.*`
+        let txt = `      🧣 *Support Group* 🧣\n\n*${botName}* is 𝚃𝙾𝙼𝙰𝙽 and we are always happy to help you.\n\n*Link:*https://chat.whatsapp.com/JTTwcmElglZEKmPNrg8VRA\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Atlas.*`
         await Miku.sendMessage(m.sender,{image:{url:botpic}, caption:txt},{quoted:m})
     }
 }
